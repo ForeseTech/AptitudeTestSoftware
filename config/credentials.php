76d279bb1a1e8b47f51785970232ff6fb1e96e6a
Nilesh D <nileshlund@gmail.com>
2020-10-30 11:49:50 +0530

    Stored database credentials in separate file

---
+++ config/credentials.php
@@ -0,0 +1,8 @@
+<?php
+
+// Authentication credentials
+define("DB_HOST", "localhost");
+define("DB_NAME", "cts_aptitude");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+define("DB_CHARSET", "utf8mb4");
--- includes/pdo.php
+++ includes/pdo.php
@@ -1,10 +1,6 @@
 <?php
 
-define("DB_HOST", "localhost");
-define("DB_NAME", "cts_aptitude");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
-define("DB_CHARSET", "utf8mb4");
+require_once "../../config/credentials.php";
 
 $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
 $options = [
