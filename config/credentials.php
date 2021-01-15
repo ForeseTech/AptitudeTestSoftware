5e5022de9c348fd347747cebce1a0d0b86fe6508
Nilesh D <nileshlund@gmail.com>
2021-01-15 10:50:15 +0530

    feat: Modify DB Credentials

--- config/credentials.php
+++ config/credentials.php
@@ -1,8 +1,23 @@
 <?php
 
 // Authentication credentials
-define("DB_HOST", "localhost");
-define("DB_NAME", "mocks");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
+// define("DB_HOST", "localhost");
+// define("DB_NAME", "mocks");
+// define("DB_USERNAME", "root");
+// define("DB_PASSWORD", "");
+// define("DB_CHARSET", "utf8mb4");
+
+// Authentication credentials
+$cleardb_url = parse_url("mysql://b8e863b37cd37f:fd33150d@us-cdbr-east-02.cleardb.com/heroku_2193c3db1c84d74?reconnect=true");
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+
+define("DB_HOST", $cleardb_server);
+define("DB_NAME", $cleardb_db);
+define("DB_USERNAME", $cleardb_username);
+define("DB_PASSWORD", $cleardb_password);
 define("DB_CHARSET", "utf8mb4");
+
+?>
