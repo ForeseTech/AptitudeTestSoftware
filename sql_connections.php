19bbffadcce329fdaf580d52ce9d95311e2fd214
Nilesh D <nileshlund@gmail.com>
2021-01-20 21:52:50 +0530

    Update DB credentials

--- sql_connections.php
+++ sql_connections.php
@@ -1,11 +1,19 @@
 <?php
 
 function getConn() {
-  $DB_HOST =  "remotemysql.com";
-  $DB_NAME =  "3f3XkfQeQr";
-  $DB_USERNAME =  "3f3XkfQeQr";
-  $DB_PASSWORD =  "ZRB7dBQCqd";
-  $DB_CHARSET =  "utf8mb4";
+    // Authentication credentials
+   $cleardb_url = parse_url("mysql://b9ad6106fda39e:7a6d2989@us-cdbr-east-03.cleardb.com/heroku_01cda5d379b4d85?reconnect=true");
+   $DB_HOST = $cleardb_url["host"];
+   $DB_USERNAME = $cleardb_url["user"];
+   $DB_PASSWORD = $cleardb_url["pass"];
+   $DB_NAME = substr($cleardb_url["path"], 1);
+   $DB_CHARSET = "utf8mb4";
+
+   // $DB_HOST =  "remotemysql.com";
+   // $DB_NAME =  "3f3XkfQeQr";
+   // $DB_USERNAME =  "3f3XkfQeQr";
+   // $DB_PASSWORD =  "ZRB7dBQCqd";
+   // $DB_CHARSET =  "utf8mb4";
 
   $dsn = "mysql:host=" . $DB_HOST . ";dbname=" . $DB_NAME . ";charset=" . $DB_CHARSET;
   $options = [
