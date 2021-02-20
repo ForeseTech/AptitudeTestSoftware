50126c2cf6743889476cd1af998d2add62211bed
Nilesh D <nileshlund@gmail.com>
2021-02-20 23:01:25 +0530

    Update Leaderboard

--- leaderboard/index.php
+++ leaderboard/index.php
@@ -4,7 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
+  <!-- Bootswatch CSS -->
+  <link rel="stylesheet" href="https://bootswatch.com/4/cosmo/bootstrap.css" />
+
+  <!-- DataTables Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css" />
+
+  <!-- Custom CSS -->
   <link rel="stylesheet" href="../static/css/leaderboard.css">
   
   <title>Leaderboard</title>
@@ -14,15 +20,16 @@
     <table class="table table-bordered table-hover" id="scores">
       <thead>
         <tr>
-          <th>SNO</th>
-          <th>Register Number</th>
+          <th>Register Num.</th>
           <th>Name</th>
-          <th>Department</th>
-          <th>Section</th>
-          <th>Section 1</th>
-          <th>Section 2</th>
-          <th>Section 3</th>
-          <th>Section 4</th>
+          <th>Dept.</th>
+          <th>Sec.</th>
+          <th>E-Mail ID</th>
+          <th>Preference</th>
+          <th>Sec. 1</th>
+          <th>Sec. 2</th>
+          <th>Sec. 3</th>
+          <th>Sec. 4</th>
           <th>Total</th>
         </tr>
       </thead>
@@ -34,25 +41,26 @@
           require '../sql_connections.php';
           $conn = getConn();
 
-          $query = "SELECT scores.SNO, scores.reg_no, users.name, users.dept, users.section, scores.sec_1, scores.sec_2, scores.sec_3, scores.sec_4, scores.total FROM scores, users WHERE scores.reg_no = users.reg_no";
+          $query = "SELECT scores.SNO, scores.reg_no, users.name, users.dept, users.section, users.email, users.preference, scores.sec_1, scores.sec_2, scores.sec_3, scores.sec_4, scores.total FROM scores, users WHERE scores.reg_no = users.reg_no";
 
           $stmt = $conn->query($query);
-          $scores = $stmt->fetchAll();
+          $students = $stmt->fetchAll();
 
         ?>
 
-        <?php foreach($scores as $key=>$score) : ?>
+        <?php foreach($students as $key => $student) : ?>
           <tr>
-            <td><?= $key + 1 ?></td>
-            <td><?= $score["reg_no"]; ?></td>
-            <td><?= $score["name"]; ?></td>
-            <td><?= $score["dept"]; ?></td>
-            <td><?= $score["section"]; ?></td>
-            <td><?= $score["sec_1"]; ?></td>
-            <td><?= $score["sec_2"]; ?></td>
-            <td><?= $score["sec_3"]; ?></td>
-            <td><?= $score["sec_4"]; ?></td>
-            <td><?= $score["total"]; ?></td>
+            <td><?= $student["reg_no"]; ?></td>
+            <td><?= $student["name"]; ?></td>
+            <td><?= $student["dept"]; ?></td>
+            <td><?= $student["section"]; ?></td>
+            <td><?= $student["email"]; ?></td>
+            <td><?= $student["preference"]; ?></td>
+            <td><?= $student["sec_1"]; ?></td>
+            <td><?= $student["sec_2"]; ?></td>
+            <td><?= $student["sec_3"]; ?></td>
+            <td><?= $student["sec_4"]; ?></td>
+            <td><?= $student["total"]; ?></td>
           </tr>
         <?php endforeach; ?>
 
@@ -65,4 +73,19 @@
       <b>FORESE Tech</b>
     </footer>
 </body>
+<!-- jQuery -->
+  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+
+  <!-- DataTablesJS -->
+  <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+
+  <!-- DataTables for Bootstrap -->
+  <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>
+
+  <!-- Initialize Datatables -->
+  <script>
+    $(document).ready(function () {
+      $("#scores").DataTable({paging: false});
+    });
+  </script>
 </html>
