eb99ab3fc699f0c900b6b2201cc64d3230c883ac
NileshMSD <nilesh@madstreetden.com>
2022-02-02 09:05:46 +0530

    SQL Connection Credentials

--- sql_connections.php
+++ sql_connections.php
@@ -1,19 +1,19 @@
 <?php
 
 function getConn() {
-// Authentication credentials
-// $cleardb_url = parse_url("my_sql_url");
-// $DB_HOST = $cleardb_url["host"];
-// $DB_USERNAME = $cleardb_url["user"];
-// $DB_PASSWORD = $cleardb_url["pass"];
-// $DB_NAME = substr($cleardb_url["path"], 1);
-// $DB_CHARSET = "utf8mb4";
+    // Authentication credentials
+    $cleardb_url = parse_url("my_sql_url");
+    $DB_HOST = $cleardb_url["host"];
+    $DB_USERNAME = $cleardb_url["user"];
+    $DB_PASSWORD = $cleardb_url["pass"];
+    $DB_NAME = substr($cleardb_url["path"], 1);
+    $DB_CHARSET = "utf8mb4";
 
-  $DB_HOST =  "localhost";
-  $DB_NAME =  "mocks";
-  $DB_USERNAME =  "root";
-  $DB_PASSWORD =  "";
-  $DB_CHARSET =  "utf8mb4";
+//   $DB_HOST =  "localhost";
+//   $DB_NAME =  "mocks";
+//   $DB_USERNAME =  "root";
+//   $DB_PASSWORD =  "";
+//   $DB_CHARSET =  "utf8mb4";
 
   $dsn = "mysql:host=" . $DB_HOST . ";dbname=" . $DB_NAME . ";charset=" . $DB_CHARSET;
   $options = [
