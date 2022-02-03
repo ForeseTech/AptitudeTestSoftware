6fe86ef848e4f52e0042c197c4efca7b07deaaa8
NileshMSD <nilesh@madstreetden.com>
2022-02-03 19:12:41 +0530

    Remove logic for section input, add Semantic UI specific JS

--- static/js/loginForm.js
+++ static/js/loginForm.js
@@ -1,23 +1,15 @@
+// Delete any existing countdown stored
 localStorage.removeItem('savedCountdown');
 
-dept = document.forms['loginForm']['deptIn'];
-section = document.forms['loginForm']['sectionInput'];
+// UI Dropdown
+$('.ui.dropdown').dropdown();
 
-deptsWithTwoSections = ['CHE', 'EEE', 'INT', 'MEC'];
-deptsWithThreeSections = ['CSE', 'ECE'];
-
-dept.addEventListener('change', function () {
-  if (deptsWithThreeSections.includes(this.value)) {
-    section.classList.remove('d-none');
-    section.options[3].classList.remove('d-none');
-    section.required = true;
-  } else if (deptsWithTwoSections.includes(this.value)) {
-    section.classList.remove('d-none');
-    section.options[3].classList.add('d-none');
-    section.required = true;
-  } else {
-    section.classList.add('d-none');
-    section.value = '';
-    section.required = false;
-  }
+// Form Validation
+$('.ui.form').form({
+  fields: {
+    name: 'empty',
+    regIn: ['empty', 'regExp[/^19[0-9]{7}$/]'],
+    dept: 'empty',
+    email: ['empty', 'regExp[/^2019.+@svce.ac.in$/]'],
+  },
 });
