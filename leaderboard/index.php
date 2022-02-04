a014c8587f7244c5eebb06ab7e2669a539845383
NileshMSD <nilesh@madstreetden.com>
2022-02-04 18:12:27 +0530

    Update Leaderboard

--- leaderboard/index.php
+++ leaderboard/index.php
@@ -1,88 +1,107 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <!-- Bootswatch CSS -->
-  <link rel="stylesheet" href="https://bootswatch.com/4/cosmo/bootstrap.css" />
+	<!-- Semantic UI CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fomantic-ui/2.8.8/semantic.min.css">
 
-  <!-- DataTables Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css" />
+	<link rel="stylesheet" href="https://cdn.datatables.net/1.11.4/css/dataTables.semanticui.min.css">
 
-  <!-- Custom CSS -->
-  <link rel="stylesheet" href="../static/css/leaderboard.css">
-  
-  <title>Leaderboard</title>
-</head>
-<body>
-  <div class="container mt-5">
-    <table class="table table-bordered table-hover" id="scores">
-      <thead>
-        <tr>
-          <th>SNo.</th>
-          <th>Register Num.</th>
-          <th>Name</th>
-          <th>Dept.</th>
-          <th>E-Mail ID</th>
-          <th>Quants</th>
-          <th>Verbal</th>
-          <th>Coding</th>
-          <th>Core</th>
-          <th>Total</th>
-        </tr>
-      </thead>
-      
-      <tbdoy>
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="../static/css/leaderboard.css">
+	
+	<title>Leaderboard</title>
+	</head>
 
-        <?php 
+	<body>
+		<div class="ui container">
+			<table class="ui celled table" id="scores">
+				<thead>
+					<tr>
+					<th>SNo.</th>
+					<th>Register Num.</th>
+					<th>Name</th>
+					<th>Dept.</th>
+					<th>E-Mail ID</th>
+					<th>Quants</th>
+					<th>Verbal</th>
+					<th>Coding</th>
+					<th>Core</th>
+					<th>Total</th>
+					<th>Action</th>
+					</tr>
+				</thead>
+			
+				<tbdoy>
 
-          require '../sql_connections.php';
-          $conn = getConn();
+					<?php 
 
-          $query = "SELECT scores.SNO, scores.reg_no, users.name, users.dept, users.email, scores.sec_1, scores.sec_2, scores.sec_3, scores.sec_4, scores.total FROM scores, users WHERE scores.reg_no = users.reg_no ORDER by users.dept";
+						require '../sql_connections.php';
+						$conn = getConn();
 
-          $stmt = $conn->query($query);
-          $students = $stmt->fetchAll();
-        ?>
+						$query = "SELECT scores.SNO, scores.reg_no, users.name, users.dept, users.email, scores.sec_1, scores.sec_2, scores.sec_3, scores.sec_4, scores.total 
+								  FROM scores, users 
+								  WHERE scores.reg_no = users.reg_no";
 
-        <?php foreach($students as $key => $student) : ?>
-          <tr>
-            <td><?= $key + 1; ?></td>
-            <td><?= $student["reg_no"]; ?></td>
-            <td><?= $student["name"]; ?></td>
-            <td><?= $student["dept"]; ?></td>
-            <td><?= $student["email"]; ?></td>
-            <td><?= $student["sec_1"]; ?></td>
-            <td><?= $student["sec_2"]; ?></td>
-            <td><?= $student["sec_3"]; ?></td>
-            <td><?= $student["sec_4"]; ?></td>
-            <td><?= $student["total"]; ?></td>
-          </tr>
-        <?php endforeach; ?>
+						$stmt = $conn->query($query);
+						$students = $stmt->fetchAll();
 
-      </tbdoy>
-    </table>
-  </div>
-  <footer>
-      Copyright &copy; 2022 <b>FOR</b>um for <b>E</b>conomic <b>S</b>tudies by <b>E</b>ngineers - Designed and Developed
-      by
-      <b>FORESE Tech</b>
-    </footer>
-</body>
-  <!-- jQuery -->
-  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+					?>
 
-  <!-- DataTablesJS -->
-  <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+					<?php foreach($students as $key => $student) : ?>
 
-  <!-- DataTables for Bootstrap -->
-  <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>
+						<tr>
+							<td><?= $key + 1; ?></td>
+							<td><?= $student["reg_no"]; ?></td>
+							<td><?= $student["name"]; ?></td>
+							<td><?= $student["dept"]; ?></td>
+							<td><?= $student["email"]; ?></td>
+							<td><?= $student["sec_1"]; ?></td>
+							<td><?= $student["sec_2"]; ?></td>
+							<td><?= $student["sec_3"]; ?></td>
+							<td><?= $student["sec_4"]; ?></td>
+							<td><?= $student["total"]; ?></td>
+							<td>
+								<form action="delete.php" method="POST">
+									<input type="hidden" name="sno" value="<?= $student["SNO"]; ?>">
+									<button type="submit" style="background-color: white;border:none;cursor:pointer">
+										<i class="icon trash alternate red large"></i>
+									</button>
+								</form>
+							</td>
+						</tr>
 
-  <!-- Initialize Datatables -->
-  <script>
-    $(document).ready(function () {
-      $("#scores").DataTable({paging: false});
-    });
-  </script>
+					<?php endforeach; ?>
+
+				</tbdoy>
+			</table>
+		</div>
+
+		<footer>
+		Copyright &copy; 2022 <b>FOR</b>um for <b>E</b>conomic <b>S</b>tudies by <b>E</b>ngineers - Designed and Developed
+		by
+		<b>FORESE Tech</b>
+		</footer>
+
+	</body>
+
+	<!-- jQuery -->
+	<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+
+	<!-- DataTables JS -->
+	<script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+
+	<script src="https://cdn.datatables.net/1.11.4/js/dataTables.semanticui.min.js"></script>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/fomantic-ui/2.8.8/semantic.min.js"></script>
+	
+	<!-- Initialize Datatables -->
+	<script>
+		$(document).ready(function () {
+			$("#scores").DataTable({paging: false});
+		});
+	</script>
+
 </html>
